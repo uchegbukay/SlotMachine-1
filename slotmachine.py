136d6b6243400ce9bca34dcafaf5aa9e52dcd96c
uchegbukay <37336643+uchegbukay@users.noreply.github.com>
2018-03-13 08:28:10 -0500

    Update slotmachine.py

--- slotmachine.py
+++ slotmachine.py
@@ -1,18 +1,18 @@
-print("Welcome to the Slot Machine!")
+print("Welcome to the Slot Machine Travel Guide!")
 numberOfTimes = input('How many times do you want to play?')
-slotsPossible = ["bar","bar","bar","cherry","crown","crown"]
+slotsPossible = ["Africa","Europe","Asia","Africa","Asia"]
 from random import *
 def play():
     slot1=choice(slotsPossible)
     slot2=choice(slotsPossible)
     slot3=choice(slotsPossible)
     win = ""
-    if (slot1==slot2==slot3=="cherry"):
-        win = "You win $100"
-    if (slot1==slot2==slot3=="crown"):
-        win = "You win $50"
-    if (slot1==slot2==slot3=="bar"):
-        win = "You win $5"
+    if (slot1==slot2==slot3=="Africa"):
+        win = "Congrats! Youre headed to Africa"
+    if (slot1==slot2==slot3=="Asia"):
+        win = "Congrats! Youre headed to Asia"
+    if (slot1==slot2==slot3=="Europe"):
+        win = "Congrats! Youre headed to Europe"
     return slot1+":"+slot2+":"+slot3+" "+win
 for i in range(int(numberOfTimes)):
     print(play())
